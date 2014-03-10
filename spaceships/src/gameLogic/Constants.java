34c7a54be36ac3574ed135e819a5b557457bf52a
Andrew Fogarty <andrew.f.fogarty@gmail.com>
2014-03-10 09:45:26 -0400

    Broke Turn180 into 2 messages

--- spaceships/src/gameLogic/Constants.java
+++ spaceships/src/gameLogic/Constants.java
@@ -26,7 +26,7 @@ public class Constants
 	public static enum OrientationType { North, East, South, West }
 	public static enum WeaponType { Cannon, Torpedo, Mine }
 	public static enum VisibilityType { None, Sonar, Radar, Both}
-	public static enum ActionType { Move, Place, FireCannon, FireTorpedo, DropMine, PickupMine, TurnLeft, TurnRight, Turn180, Repair }
+	public static enum ActionType { Move, Place, FireCannon, FireTorpedo, DropMine, PickupMine, TurnLeft, TurnRight, Turn180Left, Turn180Right, Repair }
 	public static enum ArmourType { Normal, Heavy }
 	public static enum PlayerNumber { World, PlayerOne, PlayerTwo }
 	public static enum SpaceThingType { CruiserShip, DestroyerShip, MineLayerShip, RadarBoatShip, BaseTile, TorpedoShip, Asteroid, Mine }
