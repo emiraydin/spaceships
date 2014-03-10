a09eacbfa6f09afba4462af17e126eb86745c03b
tinalatif <tina.tl.latif@gmail.com>
2014-03-10 02:58:48 -0700

    added turning for cruisers and destroyers.. hopefully?

--- spaceserver/src/logic/spacethings/CruiserShip.java
+++ spaceserver/src/logic/spacethings/CruiserShip.java
@@ -1,6 +1,11 @@
 package logic.spacethings;
 
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+
 import common.GameConstants.ActionType;
+import common.GameConstants.OrientationType;
 
 import logic.AbstractWeapon;
 import logic.GameBoard;
@@ -29,9 +34,116 @@ public class CruiserShip extends AbstractShip {
 	}
 
 	@Override
-	public boolean tryTurning(ActionType turnType) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean tryTurning(ActionType direction) {
+		if(!(direction == ActionType.TurnLeft || direction == ActionType.TurnRight)) { 
+			System.out.println("CruiserShip cannot turn in direction: " + direction.name());
+			return false;
+		}
+		
+		List<Point> obstaclesInTurnZone = getObstaclesInTurnZone(direction);
+		
+		// if obstaclesInTurnZone returned null, it means the turn puts the ship out of bounds!
+		if(obstaclesInTurnZone == null) { 
+			System.out.println("Turn not completed because out of bounds");
+			return false;
+		}
+		
+		return handleObstaclesWhileTurning(obstaclesInTurnZone);		
+	}
+	
+	/**
+	 * Gets the coordinates for all the obstacles in the turn zone. 
+	 * @param turnDirection The direction of the desired turn.
+	 * @return null if the turn puts the ship out of bounds,
+	 *         otherwise a list of the coordinates of obstacles in the way of the turn
+	 *         (ships are multiple coordinates for multiple collisions).
+	 */
+	protected List<Point> getObstaclesInTurnZone(ActionType direction) {
+		List<Point> obstacles = new ArrayList<Point>();
+		
+		int x = this.getX();
+		int y = this.getY();
+		OrientationType orientation = this.getOrientation();
+		// THESE USE NEW ORIGIN CONVENTION
+		
+		/* First, add the blocks in the way (not in final position of ship) */
+		
+		// top-left quadrant blocks
+		if((orientation == OrientationType.West && direction == ActionType.TurnRight)
+				|| (orientation == OrientationType.North && direction == ActionType.TurnLeft)) { 							
+			for(int j = length-1; j >= 1; j--) { 
+				for(int i = 1; i <= length-j; i++) {
+					if(!this.addObstacleToList(x-i, y+j, obstacles)) { 
+						return null;
+					}
+				}
+			}
+		}
+		// top-right quadrant blocks
+		else if((orientation == OrientationType.East && direction == ActionType.TurnLeft)
+				|| (orientation == OrientationType.North && direction == ActionType.TurnRight)) { 
+			for(int j = length-1; j >= 1; j--) { 
+				for(int i = 1; i <= length-j; i++) {
+					if(!this.addObstacleToList(x+i, y+j, obstacles)) { 
+						return null;
+					}
+				}
+			}
+		}
+		// bottom-left quadrant blocks
+		else if((orientation == OrientationType.West && direction == ActionType.TurnLeft)
+				|| (orientation == OrientationType.South && direction == ActionType.TurnRight)) { 
+			for(int j = length-1; j >= 1; j--) { 
+				for(int i = 1; i <= length-j; i++) {
+					if(!this.addObstacleToList(x-i, y-j, obstacles)) { 
+						return null;
+					}
+				}
+			}
+		}
+		// bottom-right quadrant blocks
+		else if((orientation == OrientationType.East && direction == ActionType.TurnRight)
+				|| (orientation == OrientationType.South && direction == ActionType.TurnLeft)) { 
+			for(int j = length-1; j >= 1; j--) { 
+				for(int i = 1; i <= length-j; i++) {
+					if(!this.addObstacleToList(x+i, y-j, obstacles)) { 
+						return null;
+					}
+				}
+			}
+		}
+		
+		/* Then add final position of ship */
+		
+		OrientationType finalOrientation = getOrientationAfterPivot(orientation, direction);
+		switch(finalOrientation) { 
+		case East:
+			for(int i = 1; i < this.length; i++) { 
+				if(!this.addObstacleToList(x+i, y, obstacles)) { 
+					return null;
+				}
+			}
+		case West:
+			for(int i = 1; i < this.length; i++) { 
+				if(!this.addObstacleToList(x-i, y, obstacles)) { 
+					return null;
+				}
+			}
+		case North:
+			for(int i = 1; i < this.length; i++) { 
+				if(!this.addObstacleToList(x, y+i, obstacles)) { 
+					return null;
+				}
+			}
+		case South:
+			for(int i = 1; i < this.length; i++) { 
+				if(!this.addObstacleToList(x, y-i, obstacles)) { 
+					return null;
+				}
+			}
+		}
+		
+		return obstacles;
 	}
 
 }
--- spaceserver/src/logic/spacethings/DestroyerShip.java
+++ spaceserver/src/logic/spacethings/DestroyerShip.java
@@ -4,12 +4,13 @@ import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
 
-import common.GameConstants.ActionType;
-
 import logic.AbstractWeapon;
 import logic.Cannon;
 import logic.GameBoard;
 import logic.TorpedoLauncher;
+
+import common.GameConstants.ActionType;
+import common.GameConstants.OrientationType;
 
 public class DestroyerShip extends AbstractShip {
 	public DestroyerShip(int x, int y, GameBoard gameBoard){
@@ -59,16 +60,91 @@ public class DestroyerShip extends AbstractShip {
 	 *         otherwise a list of the coordinates of obstacles in the way of the turn
 	 *         (ships are multiple coordinates for multiple collisions).
 	 */
-	protected List<Point> getObstaclesInTurnZone(ActionType turnDirection) {
+	protected List<Point> getObstaclesInTurnZone(ActionType direction) {
 		List<Point> obstacles = new ArrayList<Point>();
 		
 		int x = this.getX();
 		int y = this.getY();
+		OrientationType orientation = this.getOrientation();
+		// THESE USE NEW ORIGIN CONVENTION
 		
-		// ORIENTATION FOLLOWS NEW ORIGIN CONVENTION
-		switch(this.getOrientation()) { 
+		/* First, add the blocks in the way (not in final position of ship) */
+		
+		// top-left quadrant blocks
+		if((orientation == OrientationType.West && direction == ActionType.TurnRight)
+				|| (orientation == OrientationType.North && direction == ActionType.TurnLeft)) { 							
+			for(int j = length-1; j >= 1; j--) { 
+				for(int i = 1; i <= length-j; i++) {
+					if(!this.addObstacleToList(x-i, y+j, obstacles)) { 
+						return null;
+					}
+				}
+			}
+		}
+		// top-right quadrant blocks
+		else if((orientation == OrientationType.East && direction == ActionType.TurnLeft)
+				|| (orientation == OrientationType.North && direction == ActionType.TurnRight)) { 
+			for(int j = length-1; j >= 1; j--) { 
+				for(int i = 1; i <= length-j; i++) {
+					if(!this.addObstacleToList(x+i, y+j, obstacles)) { 
+						return null;
+					}
+				}
+			}
+		}
+		// bottom-left quadrant blocks
+		else if((orientation == OrientationType.West && direction == ActionType.TurnLeft)
+				|| (orientation == OrientationType.South && direction == ActionType.TurnRight)) { 
+			for(int j = length-1; j >= 1; j--) { 
+				for(int i = 1; i <= length-j; i++) {
+					if(!this.addObstacleToList(x-i, y-j, obstacles)) { 
+						return null;
+					}
+				}
+			}
+		}
+		// bottom-right quadrant blocks
+		else if((orientation == OrientationType.East && direction == ActionType.TurnRight)
+				|| (orientation == OrientationType.South && direction == ActionType.TurnLeft)) { 
+			for(int j = length-1; j >= 1; j--) { 
+				for(int i = 1; i <= length-j; i++) {
+					if(!this.addObstacleToList(x+i, y-j, obstacles)) { 
+						return null;
+					}
+				}
+			}
 		}
 		
-		// TODO
+		/* Then add final position of ship */
+		
+		OrientationType finalOrientation = getOrientationAfterPivot(orientation, direction);
+		switch(finalOrientation) { 
+		case East:
+			for(int i = 1; i < this.length; i++) { 
+				if(!this.addObstacleToList(x+i, y, obstacles)) { 
+					return null;
+				}
+			}
+		case West:
+			for(int i = 1; i < this.length; i++) { 
+				if(!this.addObstacleToList(x-i, y, obstacles)) { 
+					return null;
+				}
+			}
+		case North:
+			for(int i = 1; i < this.length; i++) { 
+				if(!this.addObstacleToList(x, y+i, obstacles)) { 
+					return null;
+				}
+			}
+		case South:
+			for(int i = 1; i < this.length; i++) { 
+				if(!this.addObstacleToList(x, y-i, obstacles)) { 
+					return null;
+				}
+			}
+		}
+		
+		return obstacles;
 	}
 }
