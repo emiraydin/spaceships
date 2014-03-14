6b5a5dab21178e2cdc31d3d72970935805e473b9
Emir Aydin <emraydn12@gmail.com>
2014-03-14 05:33:07 -0400

    Don't display match messages

--- spaceserver/src/util/ClientThread.java
+++ spaceserver/src/util/ClientThread.java
@@ -92,9 +92,9 @@ public class ClientThread extends Thread {
 							this.matchedThread = current;
 							this.matchedThread.setPrompted(true);
 							this.setPrompted(true);
-							output.println("Waiting for " + matchUser + " to respond...");
-							this.matchedThread.output.println(clientName + " wants to play with you. Enter Y to accept, anything else to reject.");
-							this.matchedThread.output.flush();
+//							output.println("Waiting for " + matchUser + " to respond...");
+//							this.matchedThread.output.println(clientName + " wants to play with you. Enter Y to accept, anything else to reject.");
+//							this.matchedThread.output.flush();
 //							String otherUserInput = this.matchedThread.input.readLine().trim();
 							// Check if the other user confirms the match
 							if (/*otherUserInput.startsWith("Y")*/true) {
--- spaceships/src/util/Properties.java
+++ spaceships/src/util/Properties.java
@@ -3,7 +3,7 @@ package util;
 public class Properties {
 	
 	public static final int PORT_NUMBER = 2014;
-	public static final String HOST_NAME = "localhost";
+	public static final String HOST_NAME = "54.214.255.94";
 	// Server IP: 54.214.255.94
 
 }
