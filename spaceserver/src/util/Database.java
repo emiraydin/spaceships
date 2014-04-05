331a85b40a4eda8d3547dcd9c8089ab0dbf98dd1
Emir Aydin <emraydn12@gmail.com>
2014-04-05 05:50:11 -0400

    All database functions are now working (tested)

--- spaceserver/src/util/Database.java
+++ spaceserver/src/util/Database.java
@@ -27,11 +27,12 @@ public class Database {
 
 		Class.forName("com.mysql.jdbc.Driver");
 
-		connect = DriverManager.getConnection("jdbc:mysql://" + Properties.MYSQL_HOST + '/' + Properties.MYSQL_DBNAME
-				+ "/user=" + Properties.MYSQL_USER + "&password=" + Properties.MYSQL_PASSWORD);
+		connect = DriverManager.getConnection("jdbc:mysql://" + Properties.MYSQL_HOST + '/' + Properties.MYSQL_DBNAME,
+					Properties.MYSQL_USER, Properties.MYSQL_PASSWORD);
+		connect.setAutoCommit(false);
 
 	}
-
+	
 	/**
 	 * Adds a new user to the database.
 	 * @param username
@@ -45,7 +46,7 @@ public class Database {
 
 		// If the user already exists, return false
 		preparedStatement = connect.prepareStatement("SELECT COUNT(*) FROM users WHERE username=?");
-		preparedStatement.setString(0, username);
+		preparedStatement.setString(1, username);
 		ResultSet resultSet = preparedStatement.executeQuery();
 		int result = 0;
 		while (resultSet.next())
@@ -55,8 +56,8 @@ public class Database {
 		
 		// Initialize user info
 		preparedStatement = connect.prepareStatement("INSERT INTO users (username, password) VALUES (?,?)", Statement.RETURN_GENERATED_KEYS);
-		preparedStatement.setString(0, username);
-		preparedStatement.setString(1, SHA1(password));
+		preparedStatement.setString(1, username);
+		preparedStatement.setString(2, SHA1(password));
 		preparedStatement.executeUpdate();
 
 		// Get the user id just created
@@ -66,10 +67,10 @@ public class Database {
 
 		// Initialize stats data
 		preparedStatement = connect.prepareStatement("INSERT INTO stats (uid, score, wins, losses) VALUES (?, ?, ?, ?)");
-		preparedStatement.setInt(0, uid);
-		preparedStatement.setInt(1, 0);
+		preparedStatement.setInt(1, uid);
 		preparedStatement.setInt(2, 0);
 		preparedStatement.setInt(3, 0);
+		preparedStatement.setInt(4, 0);
 		preparedStatement.executeUpdate();
 		// Commit everything
 		connect.commit();
@@ -90,8 +91,8 @@ public class Database {
 	public boolean loginUser(String username, String password) throws SQLException, NoSuchAlgorithmException, UnsupportedEncodingException {
 
 		preparedStatement = connect.prepareStatement("SELECT COUNT(*) FROM users WHERE username=? AND password=?");
-		preparedStatement.setString(0, username);
-		preparedStatement.setString(1, SHA1(password));
+		preparedStatement.setString(1, username);
+		preparedStatement.setString(2, SHA1(password));
 		ResultSet resultSet = preparedStatement.executeQuery();
 		int result = 0;
 		while (resultSet.next())
@@ -100,9 +101,10 @@ public class Database {
 		if (result > 0) {
 			// Update login variable
 			preparedStatement = connect.prepareStatement("UPDATE users SET online=? WHERE username=?");
-			preparedStatement.setBoolean(0, true);
-			preparedStatement.setString(1, username);
+			preparedStatement.setBoolean(1, true);
+			preparedStatement.setString(2, username);
 			preparedStatement.executeUpdate();
+			connect.commit();
 			return true;
 		} else {
 			return false;
@@ -120,10 +122,11 @@ public class Database {
 
 		// Mark the user as offline
 		preparedStatement = connect.prepareStatement("UPDATE users SET online=? WHERE username=?");
-		preparedStatement.setBoolean(0, false);
-		preparedStatement.setString(1, username);
+		preparedStatement.setBoolean(1, false);
+		preparedStatement.setString(2, username);
 		int result = preparedStatement.executeUpdate();
-
+		connect.commit();
+		
 		if (result > -1)
 			return true;
 		else
@@ -144,10 +147,11 @@ public class Database {
 	public boolean updateUser(String username, String password) throws SQLException, NoSuchAlgorithmException, UnsupportedEncodingException {
 
 		preparedStatement = connect.prepareStatement("UPDATE users SET password=? WHERE username=?");
-		preparedStatement.setString(0, SHA1(password));
-		preparedStatement.setString(1, username);
+		preparedStatement.setString(1, SHA1(password));
+		preparedStatement.setString(2, username);
 		int result = preparedStatement.executeUpdate();
-
+		connect.commit();
+		
 		if (result > -1)
 			return true;
 		else
@@ -167,7 +171,7 @@ public class Database {
 
 		if (showOnline == 0 || showOnline == 1) {
 			preparedStatement = connect.prepareStatement("SELECT * FROM users WHERE online=?");
-			preparedStatement.setBoolean(0, showOnline != 0);
+			preparedStatement.setBoolean(1, showOnline != 0);
 		} else {
 			preparedStatement = connect.prepareStatement("SELECT * FROM users");
 		}
@@ -200,8 +204,8 @@ public class Database {
 
 		// Check if a history exists for same gid & uid
 		preparedStatement = connect.prepareStatement("SELECT COUNT(*) FROM history WHERE gid=? AND uid=?");
-		preparedStatement.setInt(0, gid);
-		preparedStatement.setInt(1, uid);
+		preparedStatement.setInt(1, gid);
+		preparedStatement.setInt(2, uid);
 		ResultSet resultSet = preparedStatement.executeQuery();
 		int result = 0;
 		while (resultSet.next())
@@ -210,17 +214,19 @@ public class Database {
 		// It already exists, so just update
 		if (result > 0) {
 			preparedStatement = connect.prepareStatement("UPDATE history SET latest=? WHERE gid=? AND uid=?");
-			preparedStatement.setString(0, message);
-			preparedStatement.setInt(1, gid);
-			preparedStatement.setInt(2, uid);
+			preparedStatement.setString(1, message);
+			preparedStatement.setInt(2, gid);
+			preparedStatement.setInt(3, uid);
 			preparedStatement.executeUpdate();
+			connect.commit();
 		} else {
 			// It doesn't exist, add a new history item
 			preparedStatement = connect.prepareStatement("INSERT INTO history (gid, uid, latest) VALUES (?, ?, ?)");
-			preparedStatement.setInt(0, gid);
-			preparedStatement.setInt(1, uid);
-			preparedStatement.setString(2, message);
+			preparedStatement.setInt(1, gid);
+			preparedStatement.setInt(2, uid);
+			preparedStatement.setString(3, message);
 			preparedStatement.executeUpdate();
+			connect.commit();
 		}
 
 		return true;
@@ -239,8 +245,8 @@ public class Database {
 	public NewTurnMessage getHistory(int gid, int uid) throws SQLException, ClassNotFoundException, IOException {
 
 		preparedStatement = connect.prepareStatement("SELECT latest FROM history WHERE gid=? AND uid=?");
-		preparedStatement.setInt(0, gid);
-		preparedStatement.setInt(1, uid);
+		preparedStatement.setInt(1, gid);
+		preparedStatement.setInt(2, uid);
 		ResultSet resultSet = preparedStatement.executeQuery();
 
 		NewTurnMessage latest = null;
@@ -264,11 +270,12 @@ public class Database {
 	public int createGame(int uid1, int uid2, boolean active) throws SQLException {
 
 		preparedStatement = connect.prepareStatement("INSERT INTO games (uid1, uid2, active, last_updated) VALUES (?, ?, ?, now())", Statement.RETURN_GENERATED_KEYS);
-		preparedStatement.setInt(0, uid1);
-		preparedStatement.setInt(1, uid2);
-		preparedStatement.setBoolean(2, active);
+		preparedStatement.setInt(1, uid1);
+		preparedStatement.setInt(2, uid2);
+		preparedStatement.setBoolean(3, active);
 		preparedStatement.executeUpdate();
-
+		connect.commit();
+		
 		ResultSet keys = preparedStatement.getGeneratedKeys();
 		keys.next();
 		int gid = keys.getInt(1);
@@ -289,7 +296,7 @@ public class Database {
 
 		if (showActive == 0 || showActive == 1) {
 			preparedStatement = connect.prepareStatement("SELECT * FROM games WHERE active=?");
-			preparedStatement.setBoolean(0, showActive != 0);
+			preparedStatement.setBoolean(1, showActive != 0);
 		} else {
 			preparedStatement = connect.prepareStatement("SELECT * FROM games");
 		}
@@ -304,9 +311,8 @@ public class Database {
 			row[0] = Integer.toString(resultSet.getInt("gid"));
 			row[1] = Integer.toString(resultSet.getInt("uid1"));
 			row[2] = Integer.toString(resultSet.getInt("uid2"));
-			row[3] = df.format(resultSet.getDate("last_updated"));
+			row[3] = df.format(resultSet.getTimestamp("last_updated"));
 			result.add(row);
-
 		}
 
 		return result;
@@ -322,11 +328,11 @@ public class Database {
 	 */
 	public boolean updateGame(int gid, boolean active) throws SQLException {
 
-		preparedStatement = connect.prepareStatement("UPDATE games SET active=? WHERE gid=?");
-		preparedStatement.setBoolean(0, active);
-		preparedStatement.setInt(1, gid);
-
+		preparedStatement = connect.prepareStatement("UPDATE games SET active=?, last_updated=now() WHERE gid=?");
+		preparedStatement.setBoolean(1, active);
+		preparedStatement.setInt(2, gid);
 		int result = preparedStatement.executeUpdate();
+		connect.commit();
 
 		if (result > -1)
 			return true;
@@ -346,13 +352,14 @@ public class Database {
 	 */
 	public boolean updateStats(int uid, int score, int wins, int losses) throws SQLException {
 
-		preparedStatement = connect.prepareStatement("UPDATE stats SET score=? AND wins=? AND losses=? WHERE uid=?");
-		preparedStatement.setInt(0, score);
-		preparedStatement.setInt(1, wins);
-		preparedStatement.setInt(2, losses);
-		preparedStatement.setInt(3, uid);
+		preparedStatement = connect.prepareStatement("UPDATE stats SET score=?, wins=?, losses=? WHERE uid=?");
+		preparedStatement.setInt(1, score);
+		preparedStatement.setInt(2, wins);
+		preparedStatement.setInt(3, losses);
+		preparedStatement.setInt(4, uid);
 		int result = preparedStatement.executeUpdate();
-
+		connect.commit();
+		
 		if (result > -1)
 			return true;
 		else
@@ -373,12 +380,10 @@ public class Database {
 		if (showOnline == 0 || showOnline == 1) {
 			preparedStatement = connect.prepareStatement("SELECT * FROM stats JOIN users ON "
 					+ "stats.uid = users.uid WHERE users.online=?");
-			preparedStatement.setBoolean(0, showOnline != 0);
+			preparedStatement.setBoolean(1, showOnline != 0);
 		} else {
 			preparedStatement = connect.prepareStatement("SELECT * FROM stats");
 		}
-
-		preparedStatement.executeQuery();
 		ResultSet resultSet = preparedStatement.executeQuery();
 
 		ArrayList<String[]> result = new ArrayList<>();
--- spaceserver/src/util/Properties.java
+++ spaceserver/src/util/Properties.java
@@ -4,9 +4,9 @@ public class Properties {
 	
 	public static final int PORT_NUMBER = 2014;
 	public static final String SERVER_HOST = "localhost";
-	public static final String MYSQL_HOST = "54.214.255.94";
-	public static final String MYSQL_USER = "root";
+	public static final String MYSQL_HOST = "54.214.255.94:3306";
+	public static final String MYSQL_USER = "dangleuser";
 	public static final String MYSQL_DBNAME = "spaceships";
-	public static final String MYSQL_PASSWORD = "DanglingSQL2014";
+	public static final String MYSQL_PASSWORD = "wht4MKeZmPJBmBeJ";
 
 }
