7da98fa25b15e4158a37446de519758fefdb5d27
tinalatif <tina.tl.latif@gmail.com>
2014-04-07 18:24:12 -0400

    added kamikazeboat to fleetcommanders ships

--- spaceserver/src/logic/FleetCommander.java
+++ spaceserver/src/logic/FleetCommander.java
@@ -63,47 +63,46 @@ public class FleetCommander {
 		ships.add(ship1);
 		board.unplacedShips.add(ship1);
 		
-		AbstractShip ship2 = new CruiserShip(-50, 10, orientation, this, board);
+		AbstractShip ship2 = new CruiserShip(-50, 11, orientation, this, board);
 		ships.add(ship2);
 		board.unplacedShips.add(ship2);
 		
-//		AbstractShip ship3 = new CruiserShip(-50, 11, orientation, this, board);
-//		ships.add(ship3);
-//		board.fuckJava.add(ship3);
+		AbstractShip ship3 = new DestroyerShip(-50, 12, orientation, this, board);
+		ships.add(ship3);
+		board.unplacedShips.add(ship3);
 		
-		AbstractShip ship4 = new DestroyerShip(-50, 12, orientation, this, board);
+		AbstractShip ship4 = new DestroyerShip(-50, 13, orientation, this, board);
 		ships.add(ship4);
 		board.unplacedShips.add(ship4);
 		
-		AbstractShip ship5 = new DestroyerShip(-50, 13, orientation, this, board);
+		AbstractShip ship5 = new DestroyerShip(-50, 14, orientation, this, board);
 		ships.add(ship5);
 		board.unplacedShips.add(ship5);
-		
-		AbstractShip ship6 = new DestroyerShip(-50, 14, orientation, this, board);
+
+		AbstractShip ship6 = new TorpedoBoatShip(-50, 15, orientation, this, board);
 		ships.add(ship6);
 		board.unplacedShips.add(ship6);
 		
-		AbstractShip ship7 = new TorpedoBoatShip(-50, 15, orientation, this, board);
+		AbstractShip ship7 = new TorpedoBoatShip(-50, 16, orientation, this, board);
 		ships.add(ship7);
-		board.unplacedShips.add(ship7);
+		board.unplacedShips.add(ship7);		
 		
-		AbstractShip ship8 = new TorpedoBoatShip(-50, 16, orientation, this, board);
+		AbstractShip ship8 = new MineLayerShip(-50, 17, orientation, this, board);
 		ships.add(ship8);
 		board.unplacedShips.add(ship8);
 		
-		AbstractShip ship9 = new MineLayerShip(-50, 17, orientation, this, board);
+		AbstractShip ship9 = new MineLayerShip(-50, 18, orientation, this, board);
 		ships.add(ship9);
-		board.unplacedShips.add(ship9);
+		board.unplacedShips.add(ship9);		
 		
-		AbstractShip ship10 = new MineLayerShip(-50, 18, orientation, this, board);
+		AbstractShip ship10 = new RadarBoatShip(-50, 19, orientation, this, board);
 		ships.add(ship10);
 		board.unplacedShips.add(ship10);
 		
-		AbstractShip ship11 = new RadarBoatShip(-50, 19, orientation, this, board);
+		AbstractShip ship11 = new KamikazeBoatShip(-50, 19, orientation, this, board);
 		ships.add(ship11);
 		board.unplacedShips.add(ship11);
 		
-		// TODO: add kamikaze ship
 		System.out.println("========= " + fcID + " ===========");
 		for (AbstractShip ship : ships){
 			System.out.println(ship.getID() + " - " + ship.getShipType().name());
